c9391fcf96a2e6198ddb6fd96b13d3d878468f75
mitrjain96 <mit.jain@somaiya.edu>
2017-05-30 08:28:40 +0530

    Commit Series: Restoring back the App Files.
    Commit(4.0)
    File: userDetails.java

--- ChatApp/app/src/main/java/com/example/mitrjain/chatapp/userDetails.java
+++ ChatApp/app/src/main/java/com/example/mitrjain/chatapp/userDetails.java
@@ -1,5 +1,7 @@
 package com.example.mitrjain.chatapp;
 
+import android.util.Log;
+
 /**
  * Created by Mit R Jain on 05-04-2017.
  */
@@ -10,13 +12,17 @@ public class userDetails {
         String passwd;
         String email;
         String refreshedToken;
-    userDetails(String name,String contact, String passwd, String email, String refreshedToken)
+    userDetails(String str)
     {
-        this.name=name;
-        this.contact_no=contact;
-        this.email=email;
-        this.passwd=passwd;
-        this.refreshedToken=refreshedToken;
+
+        Log.d("NetworkingUserDetails",str);
+        String details[] = str.split("\\s+");
+
+        this.contact_no=details[0];
+        this.email=details[1];
+        this.name=details[2];
+        this.passwd=details[3];
+        this.refreshedToken=details[4];
     }
 
 
